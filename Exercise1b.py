c2b283277ec1a457c7a722a505eabb606243764f
John Fay <john.fay@duke.edu>
2018-10-12 15:39:30 -0400

    Initial commit of Exercise1b.py

---
+++ Exercise1b.py
@@ -0,0 +1,16 @@
+# Name: Select_Example2.py
+# Description: Select roads of Class 4 from major roads tin the gnatcatcher habitat study area
+
+# Import system modules
+import arcpy
+
+# Set workspace
+arcpy.env.workspace = "V:/ArcPyDemo1/Data/SanDiego"
+
+# Set local variables
+in_features = "majorrds.shp"
+out_feature_class = "V:/ArcPyDemo1/scratch/majorrdsClass4.shp"
+where_clause = '"CLASS" = \'4\''
+
+# Execute Select
+arcpy.Select_analysis(in_features, out_feature_class, where_clause)
