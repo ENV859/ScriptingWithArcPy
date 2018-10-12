33b3a255fc0c9731dec35aa41c2e447cb1501a85
John Fay <john.fay@duke.edu>
2018-10-12 15:47:43 -0400

    Adds code to buffer output

--- Exercise1b.py
+++ Exercise1b.py
@@ -14,3 +14,8 @@ where_clause = '"CLASS" = \'4\''
 
 # Execute Select
 arcpy.Select_analysis(in_features, out_feature_class, where_clause)
+
+# Buffer the selected roads feature class we just created
+buffRoads = "V:/ArcPyDemo1/scratch/BufferedRoads.shp"
+buffDist = "500 meters"
+arcpy.Buffer_analysis(out_feature_class,buffRoads,buffDist,'','','ALL')
